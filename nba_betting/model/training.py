33d0b76ea1d35b8d6c75c8c5a106e39ccaa9a17d
Aaditya Watwe <45186715+WatweA@users.noreply.github.com>
2022-04-24 17:37:59 -0400

    Update training code and make notebook

--- nba_betting/model/training.py
+++ nba_betting/model/training.py
@@ -1,27 +1,30 @@
-from itertools import combinations_with_replacement
+# from itertools import combinations_with_replacement
 from typing import Any, Dict, List, Tuple
 
-import numpy as np
+# import numpy as np
 import pandas as pd
 from sklearn.ensemble import VotingClassifier
 from sklearn.model_selection import GridSearchCV
 
-from nba_betting.api.data import OVERLAP_COLS, get_games_by_year
+from nba_betting.api.data import get_games_by_year, make_xy
 from nba_betting.logging.tools import logger
 from nba_betting.model.classes import NBA_MODELS
 from nba_betting.model.classes.base import (
     load_estimator_cache,
     store_estimator_cache,
 )
+from nba_betting.model.classes.linear import (
+    DefenceLogReg,
+    EfficiencyLogReg,
+    OffenceLogReg,
+)
 
 
 def preprocess_games(year: int) -> "Tuple[pd.DataFrame, pd.Series[str]]":
-    logger.info("Using data for year %s as training set", year)
-    games_df = get_games_by_year(year).reset_index()
-    cleaned_df = games_df.dropna(subset=["WL"], how="all")
+    logger.info("Processing data for year %s", year)
 
-    X = cleaned_df.drop(OVERLAP_COLS, axis=1)
-    y = cleaned_df["WL"]
+    games_df = get_games_by_year(year).reset_index()
+    X, y = make_xy(games_df)
 
     logger.info(
         "Retrieved data for year %s.\nColumns: %r\nTarget: %r",
@@ -99,16 +102,18 @@ def fit_ensemble(
     )
 
     param_grid: Dict[str, List[Any]] = {
-        "weights": list(
-            set(
-                tuple(np.divide(weight_combination, sum(weight_combination)))
-                for weight_combination in combinations_with_replacement(
-                    [i for i in range(4)], len(models)
-                )
-                if sum(weight_combination) != 0
-            )
-        )
-        # "lr__C": [1.0, 100.0], "rf__n_estimators": [20, 200]
+        # "weights": list(
+        #     set(
+        #         tuple(np.divide(weight_combination, sum(weight_combination)))
+        #         for weight_combination in combinations_with_replacement(
+        #             [i for i in range(4)], len(models)
+        #         )
+        #         if sum(weight_combination) != 0
+        #     )
+        # )
+        f"{DefenceLogReg.model_name}__C": [0.001, 0.1, 1.0, 100.0],
+        f"{OffenceLogReg.model_name}__C": [0.001, 0.1, 1.0, 100.0],
+        f"{EfficiencyLogReg.model_name}__C": [0.001, 0.1, 1.0, 100.0],
     }
     logger.info("Using grid search wih params: %r", param_grid)
     grid_search: GridSearchCV = GridSearchCV(
