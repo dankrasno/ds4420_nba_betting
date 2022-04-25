8c344e2d31f9d8cb28b54bf2f4c81a293bae8e92
Aaditya Watwe <45186715+WatweA@users.noreply.github.com>
2022-04-25 10:47:53 -0400

    Fix the code oops

--- nba_betting/analysis/visualization.py
+++ nba_betting/analysis/visualization.py
@@ -9,13 +9,13 @@ from sklearn.metrics import confusion_matrix
 def plot_conf_mat(y_true: Any, y_pred: Any, labels: List[str] = ["L", "W"]) -> None:
     # Credit to https://medium.com/@dtuk81/confusion-matrix-visualization-fc31e3f30fea
     cf_matrix = confusion_matrix(y_true, y_pred)
-    cf_matrix_df = pd.DataFrame(cf_matrix, labels=["L", "W"], labels=["L", "W"])
+    cf_matrix_df = pd.DataFrame(cf_matrix, columns=labels, index=labels)
     group_names = ["True Neg", "False Pos", "False Neg", "True Pos"]
     group_counts = ["{0:0.0f}".format(value) for value in cf_matrix.flatten()]
     group_percentages = [
         "{0:.2%}".format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)
     ]
-    labels: np.ndarray[Any, np.dtype[np.str_]] = np.asarray(
+    annotations: np.ndarray[Any, np.dtype[np.str_]] = np.asarray(
         [
             f"{name}\n{count}\n{pct}"
             for name, count, pct in zip(group_names, group_counts, group_percentages)
@@ -23,7 +23,7 @@ def plot_conf_mat(y_true: Any, y_pred: Any, labels: List[str] = ["L", "W"]) -> N
     ).reshape(2, 2)
     sns.heatmap(
         cf_matrix_df,
-        annot=labels,
+        annot=annotations,
         fmt="",
         cmap="Blues",
     )
